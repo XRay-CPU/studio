b3efe47a618ae49f41e76b02ce09404ca6ae7a17
Lytton Meer <deguzmanjuvar@gmail.com>
2025-07-12 02:18:49 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <svg xmlns="http:/

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -15,11 +15,22 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
       className={cn(props.className)}
     >
       <title>Likas Bayani Logo</title>
-      {/* Shield outline */}
-      <path d="M12 2L4 5v6c0 5 3.58 9.64 8 11 4.42-1.36 8-6 8-11V5l-8-3z" />
-      {/* Stylized leaf inside the shield */}
-      <path d="M14.5 9.5c-1-2.5-3-3-4.5-3-2.5 0-4.5 2-4.5 4.5 0 2.5 2 5.5 4.5 7 2.5-1.5 4.5-4.5 4.5-7Z" />
-      <path d="M12 15V7" />
+      {/* Outer Circle */}
+      <circle cx="12" cy="12" r="10" />
+      {/* Leaf Vein / Circuit */}
+      <path d="M12 2v4" />
+      <path d="M12 18v4" />
+      <path d="M12 12l-4 4" />
+      <path d="M12 12l4 4" />
+      <path d="M12 12l-4-4" />
+      <path d="M12 12l4-4" />
+      {/* Node Dots */}
+      <circle cx="12" cy="7" r="1" />
+      <circle cx="12" cy="17" r="1" />
+      <circle cx="7" cy="7" r="1" />
+      <circle cx="17" cy="7" r="1" />
+      <circle cx="7" cy="17" r="1" />
+      <circle cx="17" cy="17" r="1" />
     </svg>
   );
 }

0c72d234bf4897f88146441a97da93c6e4568951
Lytton Meer <deguzmanjuvar@gmail.com>
2025-07-12 02:11:23 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <svg xmlns="http:/

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -7,18 +7,23 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       fill="none"
-      stroke="currentColor"
+      stroke="hsl(var(--primary))"
       strokeWidth="1.5"
       strokeLinecap="round"
       strokeLinejoin="round"
       {...props}
       className={cn(props.className)}
     >
-      <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z" stroke="hsl(var(--primary))" />
-      <path d="M12 12c-2-2.5-2.5-6-1-7.5" stroke="hsl(var(--primary))" />
-      <path d="M12 12c2-2.5 2.5-6 1-7.5" stroke="hsl(var(--primary))" />
-      <path d="M12 12v6" stroke="hsl(var(--primary))" />
-      <path d="M10 14h4" stroke="hsl(var(--primary))" />
+      <title>Likas Bayani Logo</title>
+      {/* Nature: Central Leaf */}
+      <path d="M12 2L8 8c0 2.5.5 5.5 4 8 3.5-2.5 4-5.5 4-8L12 2z" />
+      
+      {/* People Empowerment & Blockchain: Hands made of linked blocks */}
+      <path d="M8 14H6a2 2 0 00-2 2v2a2 2 0 002 2h2v-4z" />
+      <path d="M16 14h2a2 2 0 012 2v2a2 2 0 01-2 2h-2v-4z" />
+      <path d="M6 16h-.5a2.5 2.5 0 00-2.5 2.5V20" />
+      <path d="M18 16h.5a2.5 2.5 0 012.5 2.5V20" />
+      <path d="M8 20v-1a2 2 0 012-2h4a2 2 0 012 2v1" />
     </svg>
   );
 }
